b143ba4f5eb2adf74af20b146361cd6a855a7875
Denis Stepanov <denisborisstepanov@gmail.com>
2022-06-24 23:06:19 +0300

    Add starting project structure generated with Initialzr

---
+++ src/main/java/com/github/itsdenisboris/exercises/CustomExercisesApplication.java
@@ -0,0 +1,13 @@
+package com.github.itsdenisboris.exercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomExercisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomExercisesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/itsdenisboris/exercises/CustomExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.itsdenisboris.exercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomExercisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
